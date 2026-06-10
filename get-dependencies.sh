02135efde8535439f96586868b867c5d8eb7d197
Link4Electronics <link4electronics@gmail.com>
2026-06-10 17:09:59 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    gnuplot       \
     kvantum       \
     lxqt-qtplugin \
     octave        \
