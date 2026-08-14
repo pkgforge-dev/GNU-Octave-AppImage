46608ef429f4277a88873109726d23e45d7e3721
Link4Electronics <link4electronics@gmail.com>
2026-08-14 00:01:08 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,15 +3,14 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q octave | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q octave | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=/usr/share/applications/org.octave.Octave.desktop
 export ICON=/usr/share/icons/hicolor/256x256/apps/octave.png
-export DEPLOY_QT=1
-export QT_DIR=qt6
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # The arch package incorrectly gives executacle bit to files in /usr/lib/octave/11.3.0/oct
 # These are all shared objects and not executables, this confuses quick-sharun
@@ -26,12 +25,11 @@ quick-sharun \
 	/usr/share/gnuplot \
 	/usr/lib/qt6/plugins/sqldrivers
 
-# Additional changes can be done in between here
 echo 'OCTAVE_HOME=$APPDIR' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
 quick-sharun --simple-test ./dist/*.AppImage
