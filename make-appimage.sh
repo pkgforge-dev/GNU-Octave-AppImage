9f7b939244ec7e94d274e927394a8845d82933bb
Link4Electronics <link4electronics@gmail.com>
2026-08-15 07:01:34 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q octave | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q octave | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -26,7 +26,6 @@ quick-sharun \
 	/usr/share/gnuplot \
 	/usr/lib/qt6/plugins/sqldrivers
 
-# Additional changes can be done in between here
 echo 'OCTAVE_HOME=$APPDIR' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
